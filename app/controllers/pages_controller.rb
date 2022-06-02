3b8d535d533aa976822d231b90fc53bbf0b6b890
Marine Ramillon <marine.ramillon@gmail.com>
2022-06-02 18:07:54 +0200

    adding path to home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,19 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
+  before_action :set_expo, only: [:show]
 
   def home
     @expos = Expo.all
+    difference = []
+    @expos.each do |expo|
+      difference << (expo.date_start - Date.today) if expo.date_start > Date.today
+    end
+    @next_expos = difference.min(5)
+  end
+
+  private
+
+  def set_expo
+    @expo = Expo.find(params[:id])
   end
 end
