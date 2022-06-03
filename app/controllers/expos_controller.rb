0e7f03bb1324a45c85a7f9f6448848832d51237e
Jules Boulaert <julesblrt28@gmail.com>
2022-06-03 15:45:47 +0200

    search bar done

--- app/controllers/expos_controller.rb
+++ app/controllers/expos_controller.rb
@@ -3,17 +3,6 @@ class ExposController < ApplicationController
   # skip_before_action :authenticate_user!, only: [:index, :show]bn
 
   def index
-    @expos = Expo.all
-    @expos = @expos.where(tags: params[:filters][:categories]) if params[:filters].present?
-
-    ####
-    # @tags = [params[:filters][:categories]]
-
-    # @expos = @expos.map do |expo|
-    #   expo unless (expo.tags & @tags).empty?
-    # end
-    ###
-
     # pour la search et les filtres on peut voir pour utiliser les formules ci-dessous mais en discuter avec Cyril si besoin car certaines ne fonctionneront pas forcément comme tel
     # ---------
     if params[:query].present? && params[:query] != nil
@@ -23,12 +12,15 @@ class ExposController < ApplicationController
       SQL
       @expos = Expo.where(sql_query, mot: "%#{params[:query]}%")
     # elsif params[:filters].present?
-    #   @expos = Expo.where(tags: params[:filters][:categories])
+    #   sql_query = <<~SQL
+    #     expos.tags @@ :category
+    #   SQL
+    #   @expos = Expo.where(sql_query, category: "%#{params[:filters][:categories]}%")
     else
     # elsif params[:commit].present?
-    #   @expos = policy_scope(Expo.where('category ILIKE ?', "%#{params[:commit]}"))
+    # @expos = policy_scope(Expo.where('category ILIKE ?', "%#{params[:commit]}"))
     # else
-    #   @expos = policy_scope(Expo)
+    # @expos = policy_scope(Expo)
       @expos = Expo.all
     end
     # end
