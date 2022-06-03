4d33a4fb26001fc2950bed698283d6cf2f8361bf
Lina Becerra <lina.bemu@gmail.com>
2022-06-03 18:57:38 +0200

    modification controller proposals en cours

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -1,10 +1,37 @@
 class ProposalsController < ApplicationController
+
+  def new
+    @expo = Expo.find(params[:expo_id])
+    @proposal = Proposal.new
+  end
+
   def create
+    @expo = Expo.find(params[:expo_id])
+    @proposal = Proposal.new(proposal_params)
+    @proposal.expo = @expo
+    if @proposal.save
+      redirect_to
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def index
     @expo = Expo.find(params[:expo_id])
     @proposals = @expo.proposals
-    @proposal = Proposal.new()
+    @proposal = Proposal.new
+  end
+
+  def destroy
+    @proposal = Proposal.find(params[:id])
+    @proposal.destroy
+    redirect_to
+  end
+
+
+  private
+
+  def proposal_params
+    params.require(:proposal).permit(:description, :date)
   end
 end
