9782ece3141856f77873da6dd0ca6f8639ad5ff9
Lina Becerra <lina.bemu@gmail.com>
2022-06-06 12:45:36 +0200

    date picker added js set details missing

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -2,6 +2,6 @@ class Proposal < ApplicationRecord
   belongs_to :user
   belongs_to :expo
 
-  validates :date, presence: true
+  validates :date_proposale, presence: true
   validates :description, presence: true
 end
